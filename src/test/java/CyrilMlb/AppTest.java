257c1bef3a0e15d5c975bf2a01fb7d4feacada42
CyrilMlb <malherbe@et.esiea.fr>
2019-01-21 18:37:06 +0100

    Solving issue

--- src/test/java/CyrilMlb/AppTest.java
+++ src/test/java/CyrilMlb/AppTest.java
@@ -1,36 +1,20 @@
 package CyrilMlb;
 
-import static org.junit.Assert.*; // Allows you to use directly assert methods, such as assertTrue(...), assertNull(...)
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test; // for @Test
-import org.junit.Before; // for @Before
-/**
- * Unit test for simple App.
- */
-public class AppTest extends @Test{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-	super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assert Assertions.assertThat(true).isTrue();
-    }
+	/**
+	 * Unit test for simple App.
+	 */
+	public class AppTest 
+	{
+	    /**
+	     * Rigorous Test :-)
+	     */
+	    @Test
+	    public void sayTrue()
+	    {
+		Assertions.assertThat(true).isTrue();
+	    }
+	}
 }
