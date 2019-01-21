a0d959a9ec163d7c2bb9ba9caea8b95d20f88a54
CyrilMlb <malherbe@et.esiea.fr>
2019-01-21 18:40:27 +0100

    Solving issue

--- src/test/java/CyrilMlb/AppTest.java
+++ src/test/java/CyrilMlb/AppTest.java
@@ -3,18 +3,16 @@ package CyrilMlb;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-	/**
-	 * Unit test for simple App.
-	 */
-	public class AppTest 
-	{
-	    /**
-	     * Rigorous Test :-)
-	     */
-	    @Test
-	    public void sayTrue()
-	    {
-		Assertions.assertThat(true).isTrue();
-	    }
-	}
+/**
+ * Unit test for simple App.
+ */
+public class AppTest{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void sayTrue()
+    {
+	Assertions.assertThat(true).isTrue();
+    }
 }
